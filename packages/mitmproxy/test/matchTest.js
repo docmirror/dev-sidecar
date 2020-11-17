7713bffdc018a4bf9da5620457f8227ca472464d
xiaojunnuo <xiaojunnuo@qq.com>
2020-11-17 18:49:46 +0800

    docs: 小修改

--- packages/mitmproxy/test/matchTest.js
+++ packages/mitmproxy/test/matchTest.js
@@ -2,3 +2,6 @@ const name = '/docmirror/dev-sidecar/raw/master/doc/index.png'
 // https://raw.fastgit.org/docmirror/dev-sidecar/master/doc/index.png
 const ret = name.replace(/^(.+)\/raw\/(.+)$/, 'raw.fastgit.org$1/$2')
 console.log(ret)
+
+const reg = new RegExp('^/[^/]+/[^/]+$')
+console.log('/greper/d2-crud-plus/blob/master/.eslintignore'.match(reg))
