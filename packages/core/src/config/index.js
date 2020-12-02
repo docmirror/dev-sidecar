a8ef2bcf66441d5c63a735d487dcb95c0bce8a41
xiaojunnuo <xiaojunnuo@qq.com>
2020-12-02 18:04:36 +0800

    refactor: 小修改

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -78,11 +78,11 @@ module.exports = {
       },
       // google cdn
       'www.google.com': {
-        '/recaptcha/.*': { proxy: 'www.recaptcha.net' }
-        // '.*': {
-        //   proxy: 'gg.docmirror.top/_yxorp',
-        //   desc: '呀，被你发现了，偷偷的用，别声张'
-        // }
+        '/recaptcha/.*': { proxy: 'www.recaptcha.net' },
+        '.*': {
+          proxy: 'gg.docmirror.top/_yxorp',
+          desc: '呀，被你发现了，偷偷的用，别声张'
+        }
       },
       'ajax.googleapis.com': {
         '.*': {
