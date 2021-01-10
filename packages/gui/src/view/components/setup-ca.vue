7d0e14e2cac4d57d91395be6ba33ecc4148f8d81
xiaojunnuo <xiaojunnuo@qq.com>
2021-01-10 19:15:24 +0800

    refactor: doc

--- packages/gui/src/view/components/setup-ca.vue
+++ packages/gui/src/view/components/setup-ca.vue
@@ -15,15 +15,16 @@
       <a-button type="primary" style="float:right" @click="doSetup()">点此去安装</a-button>
     </template>
     <div>
-      <b>本应用正常使用必须信任本地自动随机生成的根证书</b><br/>
-      1、点击右上角“点此去安装按钮”<br/>
+      <b>本应用正常使用必须安装和信任CA根证书</b>，该证书是应用启动时本地随机生成的<br/>
       <template v-if="this.systemPlatform === 'mac'">
-        2、然后按如下图步骤将随机生成的根证书设置为信任<br/>
+        1、点击右上角“点此去安装按钮”，打开钥匙串<br/>
+        2、然后按如下图步骤将随机生成的根证书设置为始终信任<br/>
       </template>
       <template v-else>
+        1、点击右上角“点此去安装按钮”，打开证书<br/>
         2、然后按如下图步骤将根证书添加到<b>信任的根证书颁发机构</b><br/>
       </template>
-
+      3、可能需要重新启动应用和浏览器才能生效<br/>
     </div>
     <img width="100%" :src="setupImage" />
 
