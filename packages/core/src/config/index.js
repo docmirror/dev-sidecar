8e091b1185e272ccd492b71e6d033323d9460351
王良 <841369634@qq.com>
2024-04-01 10:45:00 +0800

    拦截配置调整。

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -124,10 +124,6 @@ module.exports = {
       'private-user-images.githubusercontent.com': {
         '.*': {
           sni: 'baidu.com'
-        },
-        '^/.*\\.png(\\?.*)?$': {
-          cacheDays: 365,
-          desc: '用户在PR或issue等内容中上传的图片，缓存1年。注：每张图片都有唯一的ID，不会重复，可以安心缓存'
         }
       },
       'avatars.githubusercontent.com': {
