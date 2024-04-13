5d61e4dfe1e748763d92066d3906dd4a093cd5b5
王良 <841369634@qq.com>
2024-04-14 01:29:54 +0800

    feature: `script` 拦截器，支持绝对地址和相对地址配置，该拦截器不再受限于DS的发版了。 (#294)

--- packages/mitmproxy/src/lib/interceptor/impl/res/script.js
+++ packages/mitmproxy/src/lib/interceptor/impl/res/script.js
@@ -5,11 +5,10 @@ function getScript (key, script) {
   const scriptUrl = contextPath + key
 
   const hash = CryptoJs.SHA256(script).toString(CryptoJs.enc.Base64)
-  return `
-      <script crossorigin="anonymous" defer="defer"   type="application/javascript" 
-      integrity="sha256-${hash}" 
-      src="${scriptUrl}"></script>
-      `
+  return `<script crossorigin="anonymous" defer="defer" type="application/javascript" src="${scriptUrl}" integrity="sha256-${hash}"></script>`
+}
+function getScriptByUrlOrPath (scriptUrlOrPath) {
+  return `<script crossorigin="anonymous" defer="defer" type="application/javascript" src="${scriptUrlOrPath}"></script>`
 }
 
 module.exports = {
@@ -17,25 +16,42 @@ module.exports = {
   priority: 202,
   responseIntercept (context, interceptOpt, req, res, proxyReq, proxyRes, ssl, next) {
     const { rOptions, log, setting } = context
+
+    // github特殊处理
+    if (rOptions.hostname === 'github.com' && rOptions.headers['turbo-frame'] === 'repo-content-turbo-frame') {
+      return
+    }
+
     let keys = interceptOpt.script
     if (typeof keys === 'string') {
       keys = [keys]
     }
     try {
       const scripts = monkey.get(setting.script.dirAbsolutePath)
-      let tags = getScript('global', scripts.global.script)
+      let tags = '\r\n\t' + getScript('global', scripts.global.script)
       for (const key of keys) {
-        const script = scripts[key]
-        if (script == null) {
+        if (key === 'global') {
           continue
         }
-        const scriptTag = getScript(key, script.script)
-        tags += '\r\n' + scriptTag
+
+        let scriptTag
+
+        if (key.indexOf('/') >= 0) {
+          scriptTag = getScriptByUrlOrPath(key) // 1.绝对地址或相对地址（注意：当目标站点限制跨域脚本时，可使用相对地址，再结合proxy拦截器进行代理，可规避掉限制跨域脚本问题。）
+        } else {
+          const script = scripts[key]
+          if (script == null) {
+            continue
+          }
+          scriptTag = getScript(key, script.script) // 2.DS内置脚本
+        }
+
+        tags += '\r\n\t' + scriptTag
       }
       res.setHeader('DS-Script-Interceptor', 'true')
       log.info('script response intercept: insert script', rOptions.hostname, rOptions.path, ', head:', tags)
       return {
-        head: tags
+        head: tags + '\r\n'
       }
     } catch (err) {
       res.setHeader('DS-Script-Interceptor', 'error')
