bb1f18a8ca94b87f6cc0a583c640fa968aa1a66c
王良 <841369634@qq.com>
2024-04-19 11:46:26 +0800

    github油猴脚本，链接使用高速镜像站。

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -57,8 +57,9 @@ module.exports = {
         },
         '^(/[\\w-.]+){2,}/?(\\?.*)?$': {
           script: [
-            'https://raw.githubusercontent.com/docmirror/dev-sidecar/scripts/github/monkey.js'
+            'https://mirror.ghproxy.com/https://raw.githubusercontent.com/docmirror/dev-sidecar/scripts/github/monkey.js'
           ],
+          scriptUrlDesc: '上面所使用的脚本地址，使用了高速镜像地址。',
           desc: '油猴脚本：高速下载 Git Clone/SSH、Release、Raw、Code(ZIP) 等文件 (公益加速)、项目列表单文件快捷下载、添加 git clone 命令'
         },
         // 以下三项暂时先注释掉，因为已经有油猴脚本提供高速下载地址了。
