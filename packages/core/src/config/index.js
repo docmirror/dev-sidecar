67296aed9a9c2d1c50f5d0193f353510ac4d6bc3
王良 <841369634@qq.com>
2024-04-22 15:21:35 +0800

    github油猴脚本，使用原来的文件名。

--- packages/core/src/config/index.js
+++ packages/core/src/config/index.js
@@ -59,7 +59,7 @@ module.exports = {
           // 篡改猴插件地址，以下是高速镜像地址
           tampermonkeyScript: 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/docmirror/dev-sidecar/scripts/tampermonkey.js',
           // Github油猴脚本地址，以下是高速镜像地址
-          script: 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/docmirror/dev-sidecar/scripts/github/monkey.js',
+          script: 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/docmirror/dev-sidecar/scripts/GithubEnhanced-High-Speed-Download.user.js',
           remark: '注：上面所使用的脚本地址，为高速镜像地址。',
           desc: '油猴脚本：高速下载 Git Clone/SSH、Release、Raw、Code(ZIP) 等文件 (公益加速)、项目列表单文件快捷下载、添加 git clone 命令'
         },
