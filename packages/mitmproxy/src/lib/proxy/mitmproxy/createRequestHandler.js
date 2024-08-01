aa04f998caee070a1427443c1f85459a1745b101
王良 <841369634@qq.com>
2024-08-01 18:27:11 +0800

    optimize: 优化请求处理程序，允许拦截器中异步发送请求。

--- packages/mitmproxy/src/lib/proxy/mitmproxy/createRequestHandler.js
+++ packages/mitmproxy/src/lib/proxy/mitmproxy/createRequestHandler.js
@@ -66,7 +66,7 @@ module.exports = function createRequestHandler (createIntercepts, middlewares, e
               }
               const goNext = reqIncpt.requestIntercept(context, req, res, ssl, next)
               if (goNext) {
-                next()
+                if (goNext !== 'no-next') next()
                 return
               }
             }
@@ -215,7 +215,7 @@ module.exports = function createRequestHandler (createIntercepts, middlewares, e
     (async () => {
       await requestInterceptorPromise()
 
-      if (res.writableEnded) {
+      if (res.writableEnded || res.waitingToWrite) {
         // log.info('res is writableEnded, return false')
         return false
       }
