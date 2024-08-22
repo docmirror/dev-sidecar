05ac32c794a9be23c8f36ded08a3839a83c3e14d
王良 <841369634@qq.com>
2024-08-22 13:42:48 +0800

    feature: 添加个人远程配置功能。

--- packages/core/src/config.js
+++ packages/core/src/config.js
@@ -18,12 +18,12 @@ const getDefaultConfigBasePath = function () {
   return get().server.setting.userBasePath
 }
 
-function _getRemoteSavePath (prefix = '') {
+function _getRemoteSavePath (suffix = '') {
   const dir = getDefaultConfigBasePath()
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir)
   }
-  return path.join(dir, prefix + 'remote_config.json5')
+  return path.join(dir, `remote_config${suffix}.json5`)
 }
 
 function _getConfigPath () {
@@ -51,11 +51,27 @@ const configApi = {
     await download()
     timer = setInterval(download, 24 * 60 * 60 * 1000) // 1天
   },
-  downloadRemoteConfig () {
+  async downloadRemoteConfig () {
+    if (get().app.remoteConfig.enabled !== true) {
+      // 删除保存的远程配置文件
+      configApi.deleteRemoteConfigFile()
+      configApi.deleteRemoteConfigFile('_personal')
+      return
+    }
+
+    const remoteConfig = get().app.remoteConfig
+    await configApi.doDownloadCommonRemoteConfig(remoteConfig.url)
+    await configApi.doDownloadCommonRemoteConfig(remoteConfig.personalUrl, '_personal')
+  },
+  doDownloadCommonRemoteConfig (remoteConfigUrl, suffix = '') {
     if (get().app.remoteConfig.enabled !== true) {
       return
     }
-    const remoteConfigUrl = get().app.remoteConfig.url
+    if (!remoteConfigUrl) {
+      // 删除保存的远程配置文件
+      configApi.deleteRemoteConfigFile(suffix)
+      return
+    }
     // eslint-disable-next-line handle-callback-err
     return new Promise((resolve, reject) => {
       log.info('开始下载远程配置:', remoteConfigUrl)
@@ -89,7 +105,7 @@ const configApi = {
           }
 
           if (remoteConfig != null) {
-            const remoteSavePath = _getRemoteSavePath()
+            const remoteSavePath = _getRemoteSavePath(suffix)
             fs.writeFileSync(remoteSavePath, body)
             log.info('保存远程配置文件成功:', remoteSavePath)
           } else {
@@ -111,31 +127,31 @@ const configApi = {
       })
     })
   },
-  readRemoteConfig () {
-    if (get().app.remoteConfig.enabled !== true) {
-      return {}
+  deleteRemoteConfigFile (suffix = '') {
+    const remoteSavePath = _getRemoteSavePath(suffix)
+    if (fs.existsSync(remoteSavePath)) {
+      fs.unlinkSync(remoteSavePath)
+      log.info('删除远程配置文件成功:', remoteSavePath)
     }
-    const path = _getRemoteSavePath()
-    try {
-      if (fs.existsSync(path)) {
-        const file = fs.readFileSync(path)
-        log.info('读取远程配置文件成功:', path)
-        return jsonApi.parse(file.toString())
-      } else {
-        log.warn('远程配置文件不存在:', path)
-      }
-    } catch (e) {
-      log.error('读取远程配置文件失败:', path, ', error:', e)
-    }
-
-    return {}
   },
-  readRemoteConfigStr () {
+  readRemoteConfig (suffix = '') {
+    return jsonApi.parse(configApi.readRemoteConfigStr(suffix))
+  },
+  readRemoteConfigStr (suffix = '') {
     if (get().app.remoteConfig.enabled !== true) {
+      if (suffix === '_personal') {
+        if (!get().app.remoteConfig.personalUrl) {
+          return '{}'
+        }
+      } else if (suffix === '') {
+        if (!get().app.remoteConfig.url) {
+          return '{}'
+        }
+      }
       return '{}'
     }
     try {
-      const path = _getRemoteSavePath()
+      const path = _getRemoteSavePath(suffix)
       if (fs.existsSync(path)) {
         const file = fs.readFileSync(path)
         log.info('读取远程配置文件内容成功:', path)
@@ -159,7 +175,12 @@ const configApi = {
 
     // 如果开启了远程配置，则读取远程配置，合并到默认配置中
     if (get().app.remoteConfig.enabled === true) {
-      defConfig = mergeApi.doMerge(defConfig, configApi.readRemoteConfig())
+      if (get().app.remoteConfig.url) {
+        defConfig = mergeApi.doMerge(defConfig, configApi.readRemoteConfig())
+      }
+      if (get().app.remoteConfig.personalUrl) {
+        defConfig = mergeApi.doMerge(defConfig, configApi.readRemoteConfig('_personal'))
+      }
     }
 
     // 计算新配置与默认配置（启用远程配置时，含远程配置）的差异，并保存到 config.json 中
@@ -215,7 +236,14 @@ const configApi = {
     const merged = newConfig != null ? lodash.cloneDeep(newConfig) : {}
 
     mergeApi.doMerge(merged, defConfig) // 合并默认配置
-    mergeApi.doMerge(merged, configApi.readRemoteConfig()) // 合并远程配置
+    if (get().app.remoteConfig.enabled === true) {
+      if (get().app.remoteConfig.url) {
+        mergeApi.doMerge(merged, configApi.readRemoteConfig()) // 合并共享远程配置
+      }
+      if (get().app.remoteConfig.personalUrl) {
+        mergeApi.doMerge(merged, configApi.readRemoteConfig('_personal')) // 合并个人远程配置
+      }
+    }
     if (newConfig != null) {
       mergeApi.doMerge(merged, newConfig) // 再合并一次用户配置，使用户配置重新生效
     }
