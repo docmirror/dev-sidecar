81dc4a545b5e2f0a28d40cd51818c1504771fb00
王良 <841369634@qq.com>
2024-09-09 13:25:05 +0800

    upgrade: `pac.txt`（即：`GFW` 列表） 更新：`2024-06-06 23:48:34版` -> `2024-09-02 11:03:48版`

--- packages/core/src/modules/plugin/overwall/config.js
+++ packages/core/src/modules/plugin/overwall/config.js
@@ -43,6 +43,7 @@ module.exports = {
   pac: {
     enabled: true,
     // update: [ 'https://gitlab.com/gfwlist/gfwlist/raw/master/gfwlist.txt' ],
-    pacFilePath: './extra/pac/pac.txt'
+    pacFileAbsolutePath: null, // 自定义 pac.txt 文件位置，可以是本地文件路径
+    pacFilePath: './extra/pac/pac.txt' // 默认内置 pac.txt 文件路径
   }
 }
