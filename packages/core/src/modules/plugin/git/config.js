4065acb686cbc62ff6130182da6b168a6926c791
王良 <841369634@qq.com>
2024-09-10 16:55:22 +0800

    feature: `Git.exe` 代理，添加 `是否代理HTTP请求` 的配置项 (#349)

--- packages/core/src/modules/plugin/git/config.js
+++ packages/core/src/modules/plugin/git/config.js
@@ -3,6 +3,7 @@ module.exports = {
   enabled: false,
   tip: '如果你没有安装git命令行则不需要启动它',
   setting: {
-    sslVerify: true // 是否关闭sslVerify
+    proxyHttp: false, // Git.exe 是否代理HTTP请求
+    sslVerify: true // Git.exe 是否关闭sslVerify，true=关闭 false=开启
   }
 }
