9cf4dc890ce92b402db92be25c34869bab8cf065
王良 <841369634@qq.com>
2024-09-21 21:02:52 +0800

    小调整

--- _script/1、installEnv.bat
+++ _script/1、installEnv.bat
@@ -1,4 +1,4 @@
 cd ../
 
-npm install cnpm -g --registry=https://registry.npm.taobao.org
-cnpm install lerna@6 -g
+npm install -g cnpm --registry=https://registry.npmmirror.com
+cnpm install -g lerna@6
