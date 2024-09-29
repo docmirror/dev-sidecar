1e3123e93ef8c93878d35be82ba01221c867ea2c
王良 <841369634@qq.com>
2024-09-29 12:12:28 +0800

    日志问题处理

--- packages/mitmproxy/src/lib/interceptor/impl/res/AfterOPTIONSHeaders.js
+++ packages/mitmproxy/src/lib/interceptor/impl/res/AfterOPTIONSHeaders.js
@@ -16,7 +16,7 @@ module.exports = {
     }
 
     // 替换响应头
-    if (responseReplaceApi.replaceResponseHeaders(headers, res, proxyRes)) {
+    if (responseReplaceApi.replaceResponseHeaders({ ...headers }, res, proxyRes)) {
       res.setHeader('DS-AfterOPTIONSHeaders-Interceptor', true)
       log.info('AfterOPTIONSHeaders intercept:', JSON.stringify(headers))
     }
