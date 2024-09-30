1ae0316339cf964bfcf2bb41f7f101cbe9759a0d
王良 <841369634@qq.com>
2024-09-30 09:45:28 +0800

    optimize: 系统代理排除域名功能，仅在windows环境下显示。

--- packages/gui/src/view/pages/proxy.vue
+++ packages/gui/src/view/pages/proxy.vue
@@ -30,42 +30,45 @@
           提示：仅为了加速访问<code>Github网站</code>的用户，建议不勾选。
         </div>
       </a-form-item>
-      <a-form-item v-if="isWindows()" label="设置环境变量" :label-col="labelCol" :wrapper-col="wrapperCol">
-        <a-checkbox v-model="config.proxy.setEnv" >
-          是否同时修改<code>HTTPS_PROXY</code>环境变量（不好用，不建议勾选）
-        </a-checkbox>
-        <div class="form-help">
-          当发现某些应用并没有走加速通道或加速报错时，可尝试勾选此选项，并重新开启系统代理开关<br/>
-          注意：当前已打开的命令行并不会实时生效，需要重新打开一个新的命令行窗口
-        </div>
-      </a-form-item>
-      <a-form-item v-if="isWindows()" label="设置loopback" :label-col="labelCol" :wrapper-col="wrapperCol">
-       <a-button @click="loopbackVisible=true">去设置</a-button>
-        <div class="form-help">解决<code>OneNote</code>、<code>MicrosoftStore</code>、<code>Outlook</code>等<code>UWP应用</code>开启代理后无法访问网络的问题</div>
-      </a-form-item>
-      <hr/>
-      <a-form-item label="排除国内域名" :label-col="labelCol" :wrapper-col="wrapperCol">
-        <a-checkbox v-model="config.proxy.excludeDomesticDomainAllowList" >
-          是否排除国内域名白名单
-        </a-checkbox>
-      </a-form-item>
-      <a-form-item label="自动更新国内域名" :label-col="labelCol" :wrapper-col="wrapperCol">
-        <a-checkbox v-model="config.proxy.autoUpdateDomesticDomainAllowList" >
-          是否自动更新国内域名白名单
-        </a-checkbox>
-        <div class="form-help">
-          开启自动更新并启动系统代理时，将会异步从下面的远程地址下载国内域名白名单文件到本地。<br/>
-          注：只要下载成功后，即使关闭自动更新功能，也会优先读取最近下载的文件！
-        </div>
-      </a-form-item>
-      <a-form-item label="远程国内域名地址" :label-col="labelCol" :wrapper-col="wrapperCol">
-        <a-input v-model="config.proxy.remoteDomesticDomainAllowListFileUrl" :title="config.proxy.remoteDomesticDomainAllowListFileUrl"></a-input>
-        <div class="form-help">
-          远程国内域名白名单文件内容可以是<code>base64</code>编码格式，也可以是未经过编码的
-        </div>
-      </a-form-item>
-      <a-form-item label="排除地址配置" :label-col="labelCol" :wrapper-col="wrapperCol">
-        <div>
+
+      <!-- 以下功能仅windows支持，mac和linux暂不支持 -->
+      <div v-if="isWindows()">
+        <a-form-item label="设置环境变量" :label-col="labelCol" :wrapper-col="wrapperCol">
+          <a-checkbox v-model="config.proxy.setEnv" >
+            是否同时修改<code>HTTPS_PROXY</code>环境变量（不好用，不建议勾选）
+          </a-checkbox>
+          <div class="form-help">
+            当发现某些应用并没有走加速通道或加速报错时，可尝试勾选此选项，并重新开启系统代理开关<br/>
+            注意：当前已打开的命令行并不会实时生效，需要重新打开一个新的命令行窗口
+          </div>
+        </a-form-item>
+        <a-form-item label="设置loopback" :label-col="labelCol" :wrapper-col="wrapperCol">
+         <a-button @click="loopbackVisible=true">去设置</a-button>
+          <div class="form-help">解决<code>OneNote</code>、<code>MicrosoftStore</code>、<code>Outlook</code>等<code>UWP应用</code>开启代理后无法访问网络的问题</div>
+        </a-form-item>
+        <hr/>
+        <a-form-item label="排除国内域名" :label-col="labelCol" :wrapper-col="wrapperCol">
+          <a-checkbox v-model="config.proxy.excludeDomesticDomainAllowList" >
+            是否排除国内域名白名单
+          </a-checkbox>
+        </a-form-item>
+        <a-form-item label="自动更新国内域名" :label-col="labelCol" :wrapper-col="wrapperCol">
+          <a-checkbox v-model="config.proxy.autoUpdateDomesticDomainAllowList" >
+            是否自动更新国内域名白名单
+          </a-checkbox>
+          <div class="form-help">
+            开启自动更新并启动系统代理时，将会异步从下面的远程地址下载国内域名白名单文件到本地。<br/>
+            注：只要下载成功后，即使关闭自动更新功能，也会优先读取最近下载的文件！
+          </div>
+        </a-form-item>
+        <a-form-item label="远程国内域名地址" :label-col="labelCol" :wrapper-col="wrapperCol">
+          <a-input v-model="config.proxy.remoteDomesticDomainAllowListFileUrl" :title="config.proxy.remoteDomesticDomainAllowListFileUrl"></a-input>
+          <div class="form-help">
+            远程国内域名白名单文件内容可以是<code>base64</code>编码格式，也可以是未经过编码的
+          </div>
+        </a-form-item>
+        <hr/>
+        <a-form-item label="自定义排除域名" :label-col="labelCol" :wrapper-col="wrapperCol">
           <a-row :gutter="10">
             <a-col :span="22">
               <span>访问的域名或IP符合下列配置时，将跳过系统代理</span>
@@ -82,8 +85,8 @@
               <a-button type="danger" icon="minus" @click="delExcludeIp(item,index)"/>
             </a-col>
           </a-row>
-        </div>
-      </a-form-item>
+        </a-form-item>
+      </div>
     </div>
     <template slot="footer">
       <div class="footer-bar">
