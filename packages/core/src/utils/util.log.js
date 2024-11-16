ea2d62572b38cca945ba32ebdb85a6898f8399ce
starknt <1431880400@qq.com>
2024-11-16 23:16:57 +0800

    upgrade `log4js`

--- packages/core/src/utils/util.log.js
+++ packages/core/src/utils/util.log.js
@@ -1,17 +1,4 @@
-const path = require('path')
-const log4js = require('log4js')
-const config = require('../config/index')
+const log4js = require('./util.logger')
 
-const level = process.env.NODE_ENV === 'development' ? 'debug' : 'info'
-
-function getDefaultConfigBasePath () {
-  return config.server.setting.userBasePath
-}
-
-const filename = path.join(getDefaultConfigBasePath(), '/logs/core.log')
-log4js.configure({
-  appenders: { std: { type: 'stdout' }, file: { type: 'file', pattern: 'yyyy-MM-dd', daysToKeep: 3, filename } },
-  categories: { default: { appenders: ['file', 'std'], level } },
-})
 const logger = log4js.getLogger('core')
 module.exports = logger
