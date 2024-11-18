352fb918830e219c449b1e19203b523d42265720
starknt <1431880400@qq.com>
2024-11-18 11:16:54 +0800

    eslint config

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: {
+    vueVersion: 2,
+  },
+  rules: {
+    "style/space-before-function-paren": ["error", "always"],
+    "no-console": 'off'
+  },
+  isInEditor: true,
+  ignore: [
+    '**/test/**',
+  ]
+})
