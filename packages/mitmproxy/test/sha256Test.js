952e21feb8f01ae71f1ae11a9f9d72f28a613d00
王良 <841369634@qq.com>
2024-11-20 10:07:15 +0800

    移除依赖 crypto-js

--- packages/mitmproxy/test/sha256Test.js
+++ packages/mitmproxy/test/sha256Test.js
@@ -1,5 +1,10 @@
-const CryptoJs = require('crypto-js')
+// 需要时，在 package.json 中添加以下依赖:
+// "devDependencies": {
+//     "crypto-js": "^4.2.0"
+// }
 
-const ret = CryptoJs.SHA256('111111111111')
-console.log(ret.toString(CryptoJs.enc.Base64))
-console.log(1 / 2)
+// const CryptoJs = require('crypto-js')
+//
+// const ret = CryptoJs.SHA256('111111111111')
+// console.log(ret.toString(CryptoJs.enc.Base64))
+// console.log(1 / 2)
