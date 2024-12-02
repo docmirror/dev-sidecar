345008bbceb6cfcf33d66e66ccbfefcba14b6e3d
王良 <841369634@qq.com>
2024-12-02 23:31:10 +0800

    小调整。

--- packages/gui/src/view/components/tree-node.vue
+++ packages/gui/src/view/components/tree-node.vue
@@ -15,11 +15,11 @@ export default {
 <template>
   <ul>
     <li v-for="node in treeData" :key="node.title">
-      <span v-if="node.url && (node.url.startsWith('http://') || node.url.startsWith('https://'))" :class="node.class" :style="node.style">
-        <a :title="node.tip || node.title" @click="openExternal(node.url)">{{ node.title }}</a>
+      <span v-if="node.url && (node.url.startsWith('http://') || node.url.startsWith('https://'))">
+        <a :title="node.tip || node.title" :class="node.class" :style="node.style" @click="openExternal(node.url)">{{ node.title }}</a>
       </span>
-      <span v-else :class="node.class" :style="node.style">
-        <label :title="node.tip || node.title">{{ node.title }}</label>
+      <span v-else>
+        <label :title="node.tip || node.title" :class="node.class" :style="node.style">{{ node.title }}</label>
       </span>
       <tree-node v-if="node.children && node.children.length > 0" :tree-data="node.children" class="child-node" />
     </li>
