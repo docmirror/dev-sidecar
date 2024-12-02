b905d50c39aef47bed241799e933f4241e7bcc9e
王良 <841369634@qq.com>
2024-12-02 11:46:16 +0800

    feature: 帮助页面数据，添加tip属性，鼠标移上去可以查看。

--- packages/gui/src/view/components/tree-node.vue
+++ packages/gui/src/view/components/tree-node.vue
@@ -15,10 +15,10 @@ export default {
 <template>
   <ul>
     <li v-for="node in treeData" :key="node.title">
-      <span v-if="node.url && (node.url.startsWith('http://') || node.url.startsWith('https://'))" :title="node.title">
+      <span v-if="node.url && (node.url.startsWith('http://') || node.url.startsWith('https://'))" :title="node.tip || node.title">
         <a @click="openExternal(node.url)">{{ node.title }}</a>
       </span>
-      <span v-else :title="node.title">{{ node.title }}</span>
+      <span v-else :title="node.tip || node.title">{{ node.title }}</span>
       <tree-node v-if="node.children && node.children.length > 0" :tree-data="node.children" class="child-node" />
     </li>
   </ul>
