af46d0a208496632583033cec2da87ba2e68ac04
王良 <841369634@qq.com>
2024-12-10 14:14:06 +0800

    调整帮助中心的菜单位置

--- packages/gui/src/view/router/menu.js
+++ packages/gui/src/view/router/menu.js
@@ -9,13 +9,13 @@ export default function createMenus (app) {
     { title: '加速服务', path: '/server', icon: 'thunderbolt' },
     { title: '系统代理', path: '/proxy', icon: 'deployment-unit' },
     { title: '设置', path: '/setting', icon: 'setting' },
+    { title: '帮助中心', path: '/help', icon: 'star' },
     {
       title: '应用',
       path: '/plugin',
       icon: 'api',
       children: plugins,
     },
-    { title: '帮助中心', path: '/help', icon: 'star' },
   ]
   if (app.$global && app.$global.setting && app.$global.setting.overwall) {
     plugins.push({ title: '增强功能', path: '/plugin/overwall', icon: 'global' })
