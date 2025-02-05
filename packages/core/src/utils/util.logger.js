b533427313b786a2553979f4e7336d510091753d
王良 <841369634@qq.com>
2025-02-05 13:13:25 +0800

    feature: 新增日志文件大小限制功能 (#445)

--- packages/core/src/utils/util.logger.js
+++ packages/core/src/utils/util.logger.js
@@ -29,6 +29,7 @@ const appenderConfig = {
   compress: true, // 压缩日志文件
   keepFileExt: true, // 保留日志文件扩展名为 .log
   backups: configFromFiles.app.keepLogFileCount, // 保留日志文件数
+  maxLogSize: (configFromFiles.app.maxLogFileSize || 0) * 1024 * 1024 * (configFromFiles.app.maxLogFileSizeUnit === 'GB' ? 1024 : 1), // 目前单位只有GB和MB
 }
 
 let log = null
