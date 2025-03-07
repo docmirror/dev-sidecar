a145fd993f014afc1da1f4748e52a212adeabb9c
王良 <841369634@qq.com>
2025-03-07 14:32:02 +0800

    日志小调整。

--- packages/mitmproxy/src/options.js
+++ packages/mitmproxy/src/options.js
@@ -110,7 +110,7 @@ module.exports = (serverConfig) => {
       // 配置了白名单的域名，将跳过代理
       const inWhiteList = !!matchUtil.matchHostname(whiteList, hostname, 'in whiteList')
       if (inWhiteList) {
-        log.info(`为白名单域名，不拦截: ${hostname}, headers:`, jsonApi.stringify2(req.headers))
+        log.info(`为白名单域名，不拦截: ${hostname}`)
         return false // 不拦截
       }
 
