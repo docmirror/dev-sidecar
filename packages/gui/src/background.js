c2f1d0abb52eda456af7e08623db4dcd58b73ad1
王良 <841369634@qq.com>
2025-05-15 15:50:34 +0800

    bugfix: 尝试修复macbook打开DS，图标变大的问题。

--- packages/gui/src/background.js
+++ packages/gui/src/background.js
@@ -410,7 +410,7 @@ function registerShowHideShortcut (showHideShortcut) {
 function initApp () {
   if (isMac) {
     app.whenReady().then(() => {
-      app.dock.setIcon(path.join(__dirname, '../extra/icons/512x512.png'))
+      app.dock.setIcon(path.join(__dirname, '../extra/icons/512x512-2.png'))
     })
   }
 
