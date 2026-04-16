291a078b9708da3e7db793db4fc3a2591a9487bc
王良 <841369634@qq.com>
2026-04-16 20:53:16 +0800

    feat: pip 的镜像仓库支持在共享或个人远程配置中进行配置了

--- packages/core/src/modules/plugin/pip/config.js
+++ packages/core/src/modules/plugin/pip/config.js
@@ -9,5 +9,47 @@ module.exports = {
     command: 'pip',
     trustedHost: 'pypi.org',
     registry: 'https://pypi.org/simple/', // 可以选择切换官方或者淘宝镜像
+    registryList: {
+      aliyun: {
+        name: '阿里镜像',
+        value: 'https://mirrors.aliyun.com/pypi/simple/',
+      },
+      baidu: {
+        name: '百度镜像',
+        value: 'https://mirror.baidu.com/pypi/simple/',
+      },
+      douban: {
+        name: '豆瓣镜像',
+        value: 'http://pypi.douban.com/simple/',
+      },
+      sohu: {
+        name: '搜狐镜像',
+        value: 'http://mirrors.sohu.com/Python/',
+      },
+      ustclug: {
+        name: '中科大镜像',
+        value: 'https://pypi.mirrors.ustc.edu.cn/simple/',
+      },
+      bfsu: {
+        name: '北京外国语大学镜像',
+        value: 'https://mirrors.bfsu.edu.cn/pypi/web/simple/',
+      },
+      nju: {
+        name: '南京大学镜像',
+        value: 'https://mirror.nju.edu.cn/pypi/web/simple/',
+      },
+      tsinghua: {
+        name: '清华大学镜像',
+        value: 'https://pypi.tuna.tsinghua.edu.cn/simple/',
+      },
+      hust: {
+        name: '华中科大镜像',
+        value: 'https://mirrors.hust.edu.cn/pypi/web/simple/',
+      },
+      sdut: {
+        name: '山东理工大学镜像',
+        value: 'http://pypi.sdutlinux.org/',
+      },
+    },
   },
 }
--- packages/gui/src/view/pages/plugin/pip.vue
+++ packages/gui/src/view/pages/plugin/pip.vue
@@ -68,7 +68,7 @@ export default {
             如果你的<code>pip</code>命令改成了其他名字（如<code>pip3</code>），或想设置绿色版<code>pip</code>程序路径，可在此处修改
           </div>
         </a-form-item>
-        <a-form-item label="仓库镜像" :label-col="labelCol" :wrapper-col="wrapperCol">
+        <a-form-item label="镜像仓库" :label-col="labelCol" :wrapper-col="wrapperCol">
           <a-radio-group
             v-model="config.plugin.pip.setting.registry" default-value="https://pypi.org/simple/"
             button-style="solid" @change="onSwitchRegistry"
@@ -76,35 +76,8 @@ export default {
             <a-radio-button value="https://pypi.org/simple/" title="https://pypi.org/simple/">
               原生
             </a-radio-button>
-            <a-radio-button value="https://mirrors.aliyun.com/pypi/simple/" title="https://mirrors.aliyun.com/pypi/simple/">
-              aliyun镜像
-            </a-radio-button>
-            <a-radio-button value="https://mirrors.bfsu.edu.cn/pypi/web/simple/" title="https://mirrors.bfsu.edu.cn/pypi/web/simple/">
-              北京外国语大学镜像
-            </a-radio-button>
-            <a-radio-button value="https://mirror.nju.edu.cn/pypi/web/simple/" title="https://mirror.nju.edu.cn/pypi/web/simple/">
-              南京大学镜像
-            </a-radio-button>
-            <a-radio-button value="https://pypi.tuna.tsinghua.edu.cn/simple/" title="https://pypi.tuna.tsinghua.edu.cn/simple/">
-              清华大学镜像
-            </a-radio-button>
-            <a-radio-button value="https://mirror.baidu.com/pypi/simple/" title="https://mirror.baidu.com/pypi/simple/">
-              百度镜像
-            </a-radio-button>
-            <a-radio-button value="https://pypi.mirrors.ustc.edu.cn/simple/" title="https://pypi.mirrors.ustc.edu.cn/simple/">
-              中科大镜像
-            </a-radio-button>
-            <a-radio-button value="http://pypi.douban.com/simple/" title="http://pypi.douban.com/simple/">
-              豆瓣镜像
-            </a-radio-button>
-            <a-radio-button value="http://mirrors.sohu.com/Python/" title="http://mirrors.sohu.com/Python/">
-              搜狐镜像
-            </a-radio-button>
-            <a-radio-button value="https://mirrors.hust.edu.cn/pypi/web/simple/" title="https://mirrors.hust.edu.cn/pypi/web/simple/">
-              华中科大镜像
-            </a-radio-button>
-            <a-radio-button value="http://pypi.sdutlinux.org/" title="http://pypi.sdutlinux.org/">
-              山东理工大学镜像
+            <a-radio-button v-for="(item) of config.plugin.pip.setting.registryList" :key="item.value" :value="item.value" :title="item.value">
+              {{ item.name }}
             </a-radio-button>
           </a-radio-group>
           <div class="form-help">
@@ -115,7 +88,7 @@ export default {
           <a-input v-model="config.plugin.pip.setting.trustedHost" spellcheck="false" />
           <div class="form-help">
             使用以上域名安装包时，不会进行SSL证书验证，多个域名用空格隔开<br>
-            注意：切换仓库镜像同时会修改<code>pip.ini</code>中的<code>trusted-host</code>配置，即使关闭 ds 也会继续保持
+            注意：切换镜像仓库同时会修改<code>pip.ini</code>中的<code>trusted-host</code>配置，即使关闭 ds 也会继续保持
           </div>
         </a-form-item>
       </a-form>
