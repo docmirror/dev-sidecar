4e840c0767fa885c3f2f80fbd7567bececb623bc
王良 <841369634@qq.com>
2026-04-16 12:36:38 +0800

    小调整

--- packages/core/test/urlTest.js
+++ packages/core/test/urlTest.js
@@ -1,4 +1,10 @@
 // const URL = require('node:url')
 //
-// const urlObject = new URL.URL('https://github.com:8080/aaa')
-// console.log(urlObject)
+// const url = 'https://github.com:8080/aaa?x=1#s=2'
+//
+// const urlObj = new URL.URL(url)
+// console.log('new URL.URL(url) -> ', urlObj)
+//
+// // eslint-disable-next-line node/no-deprecated-api
+// const urlObj2 = URL.parse(url)
+// console.log('\nURL.parse(url) -> ', urlObj2)
